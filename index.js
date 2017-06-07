28f9cc61f198655756138fa8e3a429cba66ddcae
isoppp <prog@d2deck.com>
2017-06-07 19:33:41 +0900

    リスト取得、削除までを追加token等を外から受け取れるように。

--- index.js
+++ index.js
@@ -1,24 +1,51 @@
 // https://api.slack.com/methods/files.list
 
+const argv = require('yargs')
+  .string('token')
+  .number('before')
+  .boolean('nodelete')
+  .argv;
+
 const requestPromise = require('request-promise');
 const oneDayMSec = 86400000;
 const getFiles = function (uri) {
-  const options = {
-    method: 'POST',
-    uri: uri,
-    body: {
-      ts_to: new Date() - oneDayMSec * 30
-    },
-    json: true
-  };
   return requestPromise(options);
 };
-getFiles('//slack.com/api/files.list')
-  .then(function (json) {
-    // Process json...
-    console.log(json);
 
+const fileListAPIoptions = {
+  method: 'POST',
+  uri: 'https://slack.com/api/files.list',
+  form: {
+    token: argv.token,
+    ts_to: Math.floor((new Date().getTime() - oneDayMSec * argv.before || 30) / 1000)
+  },
+  json: true
+};
+
+requestPromise(fileListAPIoptions)
+  .then(function (json) {
+    if (argv.nodelete) return;
+
+    return Promise.all(json.files.map(function (item) {
+      const fileDeleteAPIoptions = {
+        method: 'POST',
+        uri: 'https://slack.com/api/files.delete',
+        form: {
+          token: argv.token,
+          id: item.id
+        },
+        json: true
+      };
+
+      return requestPromise(fileDeleteAPIoptions)
+        .then(function (json) {
+        })
+        .catch(function (err) {
+          console.error(err);
+        })
+
+    }));
   })
   .catch(function (err) {
-    // Crawling failed...
+    console.error(err);
   });
